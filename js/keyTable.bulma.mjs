c63872809a650f61153d905fa6282005b8c864a5
dtbuild <dtbuild@rigil>
2026-05-07 13:45:56 +0000

    298bfb6e33530cbf13c3a35bcc8706870740e276 Dev: Switching KeyTable to Typescript and jQuery-less
    Not yet building or in any way tested
    
    840d3dfde82412636e39ffb60f5acfc853cadde5 Dev: Build for KeyTable with TS
    
    a4923db75011ec84142a08e8028257f82421a5ae New: CSS variables for controlling the focus cell styling
    Removed: SCSS build variables
    
    c791816f9e64a2cc66168c412855918ad5d4b96f Fix styling example for new CSS variables
    
    2911f33aa6fd272bd1cd4f65dab6cf2d7e95020d New: KeyTable no longer has a dependency on jQuery
    
    Dev: Tweaks for errors found by unit tests after update
    
    TS / no-jQ update finished
    
    ac472791b1b0ff85b22de13896d5404212cbf460 WIP: Inverted logic for listening to specific keys
    
    daa7da37a6a37ff26d49ad6d37d8cd00f47dba7f Dev: Examples updated to load DataTables on index pages
    
    11f6d10940b89caccf8ed79188241fe3e9576fd6 WIP: Update to use DataTables Api / Dom / util exports
    
    WIP: Update file headers to common DataTables styling
    
    WIP: Update build process to use the common `ts_extension` routine
    
    b7becb4dd0008b14e7a881906d5e1e1f027e0919 Tests: Change the animation disable to Dom
    
    59af9abcb298aeee31cfcdb0a005d63dcb6e5920 Examples: Update to remove jQuery initialisation version of each example, and focus on the Vanilla JS code for simplicity and consistency throughout the DataTables examples.
    
    6aad9e5cb66893471b804f52230f9eb78295a5ed Docs: Updated for the more consistent naming convention used for DataTables' defined types in DT3
    
    9f95544ba0e4b04074a3988a9455f993d786d481 Docs: Consistent naming for DataTables types
    
    49dfe0e648aa2208d92df6ad1587e1eb37f71f7d Update: ES6 output target from TypeScript
    
    c4ccb2d570ca3e5bfb71c497d8e85665745061c1 Dev: Not strict mode yet - that work will follow
    
    d5b2b36ac0a5c7508f908a279c2cb099c02ab176 Release 3.0.0-beta.1
    
    Sync to source repo @d5b2b36ac0a5c7508f908a279c2cb099c02ab176

--- js/keyTable.bulma.mjs
+++ js/keyTable.bulma.mjs
@@ -1,14 +1,11 @@
-/*! Bulma styling wrapper for KeyTable
- * © SpryMedia Ltd - datatables.net/license
+/*! KeyTable Bulma styling 3.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-import jQuery from 'jquery';
 import DataTable from 'datatables.net-bm';
 import KeyTable from 'datatables.net-keytable';
-
-// Allow reassignment of the $ variable
-let $ = jQuery;
 
 
 
 export default DataTable;
+
